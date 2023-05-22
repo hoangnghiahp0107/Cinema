9a470dd67b0a38d3af62bb006ed90af5e700fc95
Viet Bao <vietbao.t@gmail.com>
2023-05-22 23:45:54 +0700

    done search

--- src/modules/Home/Home.jsx
+++ src/modules/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import SearchMovies from './SearchMovies/SearchMovies';
 import PlayingMovies from './PlayingMovies/PlayingMovies';
 import ShowTimes from './ShowTimes/ShowTimes';
 import IncomingMovies from './IncomingMovies/IncomingMovies';
@@ -8,6 +9,8 @@ function Home() {
   return (
     <>
         <Banner/>
+
+        <SearchMovies/>
 
         <PlayingMovies />
 
